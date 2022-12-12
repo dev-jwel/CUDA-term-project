a0ea27764c986843efa2eeeebf674d71f41eb017
eunsi <evensomere@gmail.com>
2022-12-13 05:03:01 +0900

    as

--- src/main.cu
+++ src/main.cu
@@ -4,5 +4,15 @@
 int main(int argc, char *argv[]) {
 	printf("hello\n");
 
+	struct Edge *dev_c;
+    struct Edge dev;
+
+    cudaMalloc((void**)&dev_c, sizeof(Edge)*N);
+    sort_by_dest<<< >>>(dev_c, N); // <<< >>> 안에 뭐 넣어야 하는지 모르겠음
+    cudaMemcpy(&dev, dev_c, sizeof(Edge)*N, cudaMemcpyDeviceToHost); // dst에 대해 정렬
+
+    cudaMalloc((void**)&dev_c, sizeof(Edge)*N);
+    stable_sort_by_source<<< >>>(dev_c, N); // <<< >>> 안에 뭐 넣어야 하는지 모르겠음
+    cudaMemcpy(&dev, dev_c, sizeof(Edge)*N, cudaMemcpyDeviceToHost); // src에 대해 정렬
 	return 0;
 }
