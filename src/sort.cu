a476fb4f29f3a0e7eb40d6ad7f8e85c22fd4f44f
eunsi <evensomere@gmail.com>
2022-12-13 05:02:11 +0900

    as

--- src/sort.cu
+++ src/sort.cu
@@ -3,9 +3,29 @@
 __global__
 void sort_by_dest(const Edge *edge, size_t edge_size) {
 	// TODO
+	int temp = 0;
+    for(int i=0; i<edge_size; i++) {
+        for(int j=0; j<edge_size -i; j++) {
+            if(edge[j].to > edge[j+1].to) {
+                temp = edge[j];
+                edge[j] = edge[j+1];
+                edge[j+1] = temp;
+            }
+        }
+    }
 }
 
 __global__
 void stable_sort_by_source(const Edge *edge, size_t edge_size) {
 	// TODO
+	int temp = 0;
+	for(int i=0; i<edge_size; i++) {
+		for(int j=0; j<edge_size-i; j++) {
+			if(edge.from[j] > edge.from[j+1]) {
+				temp = edge[j];
+				edge[j] = edge[j+1];
+				edge[j+1] = temp;
+			}
+		}
+	}
 }
