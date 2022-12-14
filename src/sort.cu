f9cd6c369632ab7077993c71b6da5b2e23abb239
eunsi <evensomere@gmail.com>
2022-12-15 00:05:16 +0900

    수정

--- src/sort.cu
+++ src/sort.cu
@@ -7,13 +7,32 @@ void sort_by_dest(const Edge *edge, size_t edge_size) {
 	size_t tid = threadIdx.x + blockDim.x * blockIdx.x;
 
 	for(int i=0; i<edge_size; i++) {
+		if (i%2 == 0) {
+			if((in[tid].to > in[tid+1].to) && (tid < edge_size) && (tid %2 == 0)) {
+				temp = in[tid+1];
+				in[tid+1] = in[tid];
+				in[tid] = temp;
+			} 
+		}
+
+		else {
+			if((in[tid].to > in[tid+1].to) && (tid < edge_size) && (tid%2 !=0)) {
+				temp = in[tid+1];
+				in[tid+1] = in[tid];
+				in[tid] = temp;
+			}
+		}
+	}
+	out[tid] = in[tid];
+
+	/*for(int i=0; i<edge_size; i++) {
 		if(in[tid].to > in[tid+1].to) {
 			temp = in[tid];
 			in[tid] = in[tid + 1];
 			in[tid] = temp;
 			out[tid] = in[tid];
 		}
-	}
+	}*/
 
 	/*for(int i=0; i<edge_size; i++) {
 		for(int j=0; j<edge_size -i; j++) {
@@ -33,6 +52,25 @@ void stable_sort_by_source(const Edge *in, Edge *out, size_t edge_size) {
 	size_t tid = threadIdx.x + blockDim.x * blockIdx.x;
 
 	for(int i=0; i<edge_size; i++) {
+		if (i%2 == 0) {
+			if((in[tid].from > in[tid+1].from) && (tid < edge_size) && (tid %2 == 0)) {
+				temp = in[tid+1];
+				in[tid+1] = in[tid];
+				in[tid] = temp;
+			} 
+		}
+
+		else {
+			if((in[tid].from > in[tid+1].from) && (tid < edge_size) && (tid%2 !=0)) {
+				temp = in[tid+1];
+				in[tid+1] = in[tid];
+				in[tid] = temp;
+			}
+		}
+	}
+	out[tid] = in[tid];
+	
+	/*for(int i=0; i<edge_size; i++) {
 		if(in[tid].from > in[tid+1].from) {
 			temp = in[tid];
 			in[tid] = in[tid+1];
@@ -40,8 +78,8 @@ void stable_sort_by_source(const Edge *in, Edge *out, size_t edge_size) {
 			out[tid] = in[tid];
 		}
 	}
+	out[tid] = in[tid];*/
 
-	out[tid] = in[tid];
 	/*for(int i=0; i<edge_size; i++) {
 		for(int j=0; j<edge_size-i; j++) {
 			if(edge.from[j] > edge.from[j+1]) {
