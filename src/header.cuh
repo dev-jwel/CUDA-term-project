e62aabc2a6fd13c715cbdecb19f8165a57011e48
Jong-whi Lee <jwel.jong.whi.lee@gmail.com>
2022-12-14 16:11:57 +0900

    Add comments for function prototypes

--- src/header.cuh
+++ src/header.cuh
@@ -17,38 +17,94 @@ typedef struct {
 } Edge;
 
 __global__
-void sort_by_dest(const Edge *in, Edge out, size_t edge_size);
+void sort_by_dest(const Edge *in, Edge *out, size_t edge_size);
+/**
+ * 이 함수는 주어진 간선리스트 in을 src 기준으로 out에 정렬한다.
+ * out은 in과 같은 크기로 할당되어야 하나, 초기화될 필요는 없다.
+ */
+
 __global__
-void stable_sort_by_source(const Edge *in, Edge out, size_t edge_size);
+void stable_sort_by_source(const Edge *in, Edge *out, size_t edge_size);
+/**
+ * 이 함수는 주어진 간선리스트 in을 dest 기준으로 out에 정렬한다.
+ * out은 in과 같은 크기로 할당되어야 하나, 초기화될 필요는 없다.
+ */
 
 __global__
 void count_in_degree(const Edge *in, Count_t *out, size_t edge_size, size_t node_size);
+/**
+ * 이 함수는 src 기준으로 정렬된 간선리스트 in에서 각 노드의 들어오는 차수를 out에 기록한다.
+ * in과 out의 길이는 각각 edge_size, node_size이다.
+ * out은 초기화될 필요는 없다.
+ */
+
 __global__
 void count_out_degree(const Edge *in, Count_t *out, size_t edge_size, size_t node_size);
+/**
+ * 이 함수는 dest 기준으로 정렬된 간선리스트 in에서 각 노드의 나가는 차수를 out에 기록한다.
+ * in과 out의 길이는 각각 edge_size, node_size이다.
+ * out은 초기화될 필요는 없다.
+ */
 
 __global__
 void acc_sum(const Count_t *in, Count_t *out, size_t node_size);
+/**
+ * 이 함수는 in의 누적합을 구해 out에 기록한다.
+ * out[i] = in[0] + in[1] + ... + in[i]
+ * out은 in과 같은 크기로 할당되어야 하나, 초기화될 필요는 없다.
+ */
+
 __global__
 void element_mul(const Count_t *in1, const Count_t *in2, Count_t *out, size_t node_size);
+/**
+ * 이 함수는 in1과 in2의 각 원소를 곱한 결과를 out에 기록한다.
+ * out[i] = in1[i] * in2[i]
+ * out은 in1, in2와 같은 크기로 할당되어야 하나, 초기화될 필요는 없다.
+ */
+
 __global__
 void sum(const Count_t *in, Count_t *out, size_t node_size);
+/**
+ * 이 함수는 in의 모든 원소의 합은 out[0]에 기록한다.
+ * 병렬화를 위하여 out은 in과 동일한 크기일 필요가 있다.
+ */
 
 __inline__ __device__
-NodeIdx start_node_of_tid(const Count_t *num_candidates_by_node, size_t tid);
+NodeIdx start_node_of_tid(const Count_t *accumulated_num_candidates_by_node, size_t tid);
+/**
+ * 이 함수는 노드마다 삼각형의 후보군의 수(들어오는 차수와 나가는 차수의 곱)의 누적합을 입력으로 받는다.
+ * 주어진 tid에 대응되는 시작 노드를 찾는다.
+ */
+
 __inline__ __device__
-NodeIdx end_node_of_tid(const Count_t *num_candidates_by_node, size_t tid);
+NodeIdx end_node_of_tid(const Count_t *accumulated_num_candidates_by_node, size_t tid);
+/**
+ * 이 함수는 노드마다 삼각형의 후보군의 수(들어오는 차수와 나가는 차수의 곱)의 누적합을 입력으로 받는다.
+ * 주어진 tid에 대응되는 마지막 노드를 찾는다.
+ */
+
 __inline__ __device__
 bool has_pair(const Edge *fully_sorted_edge, Edge edge);
+/**
+ * 이 함수는 주어진 간선이 정렬된 간선리스트에 존재하는지 확인한다.
+ */
 
 __global__
 void count_triangles(
 	const Edge *dst_sorted, const Edge *src_sorted,
 	const NodeIdx *in_degree, const NodeIdx *out_degree,
-	const NodeIdx *num_candidates_by_node,
+	const NodeIdx *accumulated_num_candidates_by_node,
 	size_t node_size, size_t edge_size,
-	NodeIdx *buffer
+	Count_t *counter
 );
+/**
+ * 이 함수는 졍렬된 간선 리스트, 차수 리스트, 노드별 후보 삼각형 수의 누적합, 노드수, 간선수를 입력으로 받는다.
+ * counter는 각 스레드가 센 삼각형의 개수를 기록하며, 이 커널이 실행된 순간의 스레드 수와 길이가 같아야 한다.
+ */
 
 int naive_counter(const Edge *edges, size_t edge_size);
+/**
+ * 이 함수는 무식하게 CPU만으로 삼각형의 개수를 센다.
+ */
 
 #endif // _HEADER_CUH_
