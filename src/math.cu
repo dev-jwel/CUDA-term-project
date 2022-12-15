8094ce3287bd2d3bb47ccee9d25355316785719f
Jong-whi Lee <jwel.jong.whi.lee@gmail.com>
2022-12-15 12:22:56 +0000

    Synchronize all thread for math.cu

--- src/math.cu
+++ src/math.cu
@@ -1,26 +1,27 @@
 #include "def.cuh"
 
-__global__
-void _acc_sum(const size_t *in, size_t *out, size_t node_size) {
-	size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
-	size_t depth = 1;
-	if (tid < node_size) {
-		// copy data
-		out[tid] = in[tid];
+#include <iostream>
+#include <stdlib.h>
 
-		// scan algorithm
-		while (depth < node_size) {
-			if (tid >= 1) {
-				out[tid] += out[tid-depth];
-			}
-			depth *= 2;
-		}
+__global__
+void _acc_sum(const size_t *in, size_t *out, size_t diff, size_t node_size) {
+	size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+	if (diff <= tid && tid < node_size) {
+        out[tid] += out[tid-diff];
 	}
 }
 
 __host__
 void acc_sum(const size_t *in, size_t *out, size_t node_size) {
-    _acc_sum <<<GRID_DIM(node_size), BLOCK_DIM>>> (in, out, node_size);
+    cudaMemcpy(out, in, sizeof(size_t)*node_size, cudaMemcpyDeviceToDevice);
+
+    // scan algorithm
+
+    size_t diff = 1;
+    while (diff < node_size) {
+        _acc_sum <<<GRID_DIM(node_size), BLOCK_DIM>>> (in, out, diff, node_size);
+        diff *= 2;
+    }
 }
 
 __global__
@@ -37,26 +38,24 @@ void element_mul(const size_t *in1, const size_t *in2, size_t *out, size_t node_
 }
 
 __global__
-void _reduce_sum(const size_t *in, size_t *out, size_t node_size) {
+void _reduce_sum(const size_t *in, size_t *out, size_t node_size, bool add_last_element) {
 	size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
 	size_t buffer_size = node_size;
 
 	if (tid < node_size) {
-		// copy data
-		out[tid] = in[tid];
-
-		// sum all data
-		while (buffer_size > 1) {
-			if (tid < buffer_size/2) {
-				out[tid] += out[2*tid + buffer_size%2];
-			}
-			buffer_size += buffer_size % 2;
-			buffer_size /= 2;
-		}
+        if (tid < node_size-1 || add_last_element) {
+            out[tid] += out[tid + node_size];
+        }
 	}
 }
 
 __host__
 void reduce_sum(const size_t *in, size_t *out, size_t node_size) {
-    _reduce_sum <<<GRID_DIM(node_size), BLOCK_DIM>>> (in, out, node_size);
+    cudaMemcpy(out, in, sizeof(size_t)*node_size, cudaMemcpyDeviceToDevice);
+
+    while (node_size > 1) {
+        bool add_last_element = node_size % 2 == 0;
+        node_size = node_size / 2 + node_size % 2;
+        _reduce_sum <<<GRID_DIM(node_size), BLOCK_DIM>>> (in, out, node_size, add_last_element);
+    }
 }
