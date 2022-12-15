8f13d962a6d7a76daf2845bae0572eab1d2307c8
Jong-whi Lee <jwel.jong.whi.lee@gmail.com>
2022-12-15 13:12:09 +0900

    Implement count_triangle.cu and redesign index.cu

--- src/count_triangle.cu
+++ src/count_triangle.cu
@@ -4,9 +4,102 @@ __global__
 void count_triangles(
 	const Edge *dst_sorted, const Edge *src_sorted,
 	const NodeIdx *in_degree, const NodeIdx *out_degree,
-	const NodeIdx *num_candidates_by_node,
+	const Count_t *accumulated_num_candidates_by_node,
 	size_t node_size, size_t edge_size,
-	NodeIdx *buffer
+	Count_t *counter
 ) {
-	// TODO
+	Edge edge;
+	size_t temp, num_candidates;
+
+	size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	// split candidates by tid
+
+	size_t candidate_idx_start = tid * num_candidates_by_node / (gridDim.x * blockDim.x);
+	size_t candidate_idx_end = (tid+1) * num_candidates_by_node / (gridDim.x * blockDim.x);
+
+	// get start and end index by tid
+
+	NodeIdx node_idx_start = start_node_of_candidates(
+		accumulated_num_candidates_by_node, node_size, tid, num_threads
+	);
+	NodeIdx node_idx_end = end_node_of_candidates(
+		accumulated_num_candidates_by_node, node_size, tid, num_threads
+	);
+	size_t dst_idx = start_dst_node_index_of_edge_list(
+		dst_sorted, edge_size, node_idx_start
+	);
+	size_t src_idx = start_src_node_index_of_edge_list(
+		src_sorted, edge_size, node_idx_start
+	);
+
+	// candidates of first node
+
+	temp = candidate_idx_start - accumulated_num_candidates_by_node[node_idx_start];
+	num_candidates = in_degree[node_idx_start] * out_degree[node_idx_start];
+
+	for (size_t candidate_offset=temp; candidate_offset < num_candidates; ++candidate_offset) {
+		size_t dst_offset = temp / out_degree[node_idx_start];
+		size_t src_offset = temp % out_degree[node_idx_start];
+
+		edge.from = dst_sorted[dst_idx + dst_offset]->from;
+		edge.to = src_sorted[src_idx + src_offset]->to;
+		if (has_pair(src_sorted, edge, edge_size)) {
+			counter[tid] += 1;
+		}
+	}
+
+	// candidates of all nodes except first and last one
+
+	for (size_t node_idx=node_idx_start-1; node_idx < node_idx_end; ++node_idx) {
+		num_candidates = in_degree[node_idx] * out_degree[node_idx];
+
+		for (size_t candidate_offset=0; candidate_offset < num_candidates; ++candidate_offset) {
+			size_t dst_offset = temp / out_degree[node_idx];
+			size_t src_offset = temp % out_degree[node_idx];
+
+			edge.from = dst_sorted[dst_idx + dst_offset]->from;
+			edge.to = src_sorted[src_idx + src_offset]->to;
+			if (has_pair(src_sorted, edge, edge_size)) {
+				counter[tid] += 1;
+			}
+		}
+	}
+
+	// candidates of last node
+
+	temp = accumulated_num_candidates_by_node[node_idx_end-1] - candidate_idx_end;
+
+	for (size_t candidate_offset=0; candidate_offset < temp; ++candidate_offset) {
+		size_t dst_offset = temp / out_degree[node_idx_end];
+		size_t src_offset = temp % out_degree[node_idx_end];
+
+		edge.from = dst_sorted[dst_idx + dst_offset]->from;
+		edge.to = src_sorted[src_idx + src_offset]->to;
+		if (has_pair(src_sorted, edge, edge_size)) {
+			counter[tid] += 1;
+		}
+	}
+
+}
+
+__inline__ __device__
+void count_triangles_in_single_node(
+	const Edge *dst_sorted, const Edge *src_sorted,
+	size_t dst_start, size_t dst_end,
+	size_t src_start, size_t src_end,
+	NodeIdx in_degree, NodeIdx out_degree,
+	NodeIdx node_idx, size_t edge_size,
+	Count_t *counter
+) {
+	Edge edge;
+	for (size_t dst_idx=dst_start; dst_idx < in_degree[node_idx]; ++dst_idx) {
+		for (size_t src_idx=src_start; src_idx < out_degree[node_idx]; ++src_idx) {
+			edge.from = dst_sorted[dst_idx]->from;
+			edge.to = src_sorted[src_idx]->to;
+			if (has_pair(src_sorted, edge, edge_size)) {
+				counter[tid] += 1;
+			}
+		}
+	}
 }
--- src/index.cu
+++ src/index.cu
@@ -1,24 +1,29 @@
 #include "header.cuh"
 
-__inline__ __device__
-NodeIdx start_node_of_tid(
-	const Count_t *accumulated_num_candidates_by_node,
-	size_t node_size,
-	size_t tid,
-	size_t num_threads
+__inline__ __device__ size_t binary_search(
+	const void *array,
+	int (*compare) (const void *val1, const void *val2),
+	size_t element_size,
+	size_t array_size,
+	const void *target,
+	bool select_left
 ) {
-	Count_t num_all_candidates = accumulated_num_candidates_by_node[node_size-1];
-	Count_t target = tid * num_all_candidates / num_threads;
-	NodeIdx min = 0;
-	NodeIdx max = node_size;
-	NodeIdx mid;
+	size_t min = 0;
+	size_t max = array_size;
+	size_t mid;
 
-	while (max - min > 1) {
+	while (min < max) {
 		mid = (min + max) / 2;
-		if (accumulated_num_candidates_by_node[mid] > target) {
-			max = mid;
-		} else {
-			min = mid;
+		switch (compare(array + element_size * mid, target)) {
+			case -1: min = mid + 1; break;
+			case  1: max = mid; break;
+			case  0:
+				if (select_left) {
+					max = mid;
+				} else {
+					min = mid + 1;
+				}
+			break;
 		}
 	}
 
@@ -26,7 +31,62 @@ NodeIdx start_node_of_tid(
 }
 
 __inline__ __device__
-NodeIdx end_node_of_tid(
+int compare_src(const void *edge1, const void *edge2) {
+	if (((Edge *)edge1)->from < ((Edge *)edge2)->from) return -1;
+	if (((Edge *)edge1)->from > ((Edge *)edge2)->from) return 1;
+	return 0;
+}
+
+__inline__ __device__
+int compare_dst(const void *edge1, const void *edge2) {
+	if (((Edge *)edge1)->to < ((Edge *)edge2)->to) return -1;
+	if (((Edge *)edge1)->to > ((Edge *)edge2)->to) return 1;
+	return 0;
+}
+
+__inline__ __device__
+int compare_edge(const void *edge1, const void edge2) {
+	if (((Edge *)edge1)->from < ((Edge *)edge2)->from) return -1;
+	if (((Edge *)edge1)->from > ((Edge *)edge2)->from) return 1;
+	if (((Edge *)edge1)->to < ((Edge *)edge2)->to) return -1;
+	if (((Edge *)edge1)->to > ((Edge *)edge2)->to) return 1;
+	return 0;
+}
+
+__inline__ __device__
+int compare_count(const void *cnt1, const void cnt2) {
+	if (cnt1 < cnt2) {
+		return -1;
+	} else if (cnt1 > cnt2) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+__inline__ __device__
+size_t start_src_node_index_of_edge_list(
+	const Edge *edges,
+	size_t edge_size,
+	NodeIdx idx
+) {
+	Edge target = {idx, 0};
+	return binary_search(edges, compare_src, sizeof(Edge), edge_size, (void *) &target, true);
+}
+
+
+__inline__ __device__
+size_t start_dst_node_index_of_edge_list(
+	const Edge *edges,
+	size_t edge_size,
+	NodeIdx idx
+) {
+	Edge target = {0, idx};
+	return binary_search(edges, compare_dst, sizeof(Edge), edge_size, (void *) &target, true);
+}
+
+__inline__ __device__
+NodeIdx start_node_of_candidates(
 	const Count_t *accumulated_num_candidates_by_node,
 	size_t node_size,
 	size_t tid,
@@ -34,46 +94,30 @@ NodeIdx end_node_of_tid(
 ) {
 	Count_t num_all_candidates = accumulated_num_candidates_by_node[node_size-1];
 	Count_t target = tid * num_all_candidates / num_threads;
-	NodeIdx min = 0;
-	NodeIdx max = node_size;
-	NodeIdx mid;
 
-	while (max - min > 1) {
-		mid = (min + max) / 2;
-		if (accumulated_num_candidates_by_node[mid] < target) {
-			min = mid;
-		} else {
-			max = mid;
-		}
-	}
-
-	return max;
+	return binary_search(
+		accumulated_num_candidates_by_node, compare_count, sizeof(Count_t), node_size, (void *) &target, true
+	);
 }
 
 __inline__ __device__
-int compair_edge(Edge edge1, Edge edge2) {
-	if (edge1.from < edge2.from) return -1;
-	if (edge1.from > edge2.from) return 1;
-	if (edge1.to < edge2.to) return -1;
-	if (edge1.to > edge2.to) return 1;
-	return 0;
+NodeIdx end_node_of_candidates(
+	const Count_t *accumulated_num_candidates_by_node,
+	size_t node_size,
+	size_t tid,
+	size_t num_threads
+) {
+	Count_t num_all_candidates = accumulated_num_candidates_by_node[node_size-1];
+	Count_t target = (tid+1) * num_all_candidates / num_threads;
+
+	return binary_search(
+		accumulated_num_candidates_by_node, compare_count, sizeof(Count_t), node_size, (void *) &target, false
+	);
 }
 
 __inline__ __device__
 bool has_pair(const Edge *fully_sorted_edge, Edge edge, size_t edge_size) {
-	// TODO
-	size_t min = 0;
-	size_t max = edge_size;
-	size_t mid;
-
-	while (max > min) {
-		mid = (min + max) / 2;
-		switch (compair_edge(fully_sorted_edge[mid], edge)) {
-			case -1: min = mid; break;
-			case 1:  max = mid; break;
-			case 0:  return true;
-		}
-	}
-
-	return false;
+	Edge target = edge;
+	size_t idx = binary_search(fully_sorted_edge, compare_edge, sizeof(Edge), edge_size, (void *) &target, true);
+	return compare_edge(fully_sorted_edge[idx], target) == 0;
 }
