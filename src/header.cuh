25686428c5aeb03a309dc0279025398526d1547b
Jong-whi Lee <jwel.jong.whi.lee@gmail.com>
2022-12-15 13:12:09 +0900

    Implement count_triangle.cu and redesign index.cu

--- src/header.cuh
+++ src/header.cuh
@@ -69,8 +69,29 @@ void sum(const Count_t *in, Count_t *out, size_t node_size);
  * 병렬화를 위하여 out은 in과 동일한 크기일 필요가 있다.
  */
 
+
 __inline__ __device__
-NodeIdx start_node_of_tid(
+size_t start_src_node_index_of_edge_list(
+	const Edge *edges,
+	size_t edge_size,
+	NodeIdx idx
+);
+/**
+ * 이 함수는 src 기준 정렬된 간선리스트에서 주어진 노드의 시작 인덱스를 리턴한다.
+ */
+
+__inline__ __device__
+size_t end_dst_node_index_of_edge_list(
+	const Edge *edges,
+	size_t edge_size,
+	NodeIdx idx
+);
+/**
+ * 이 함수는 dst 기준 정렬된 간선리스트에서 주어진 노드의 시작 인덱스를 리턴한다.
+ */
+
+__inline__ __device__
+NodeIdx start_node_of_candidates(
 	const Count_t *accumulated_num_candidates_by_node,
 	size_t node_size,
 	size_t tid,
@@ -91,6 +112,7 @@ NodeIdx end_node_of_tid(
 /**
  * 이 함수는 노드마다 삼각형의 후보군의 수(들어오는 차수와 나가는 차수의 곱)의 누적합을 입력으로 받는다.
  * 주어진 tid에 대응되는 마지막 노드를 찾는다.
+ * 마지막 노드의 인덱스에 1이 더해져 있음을 유의하자.
  */
 
 __inline__ __device__
